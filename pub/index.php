6f5fa02a554a2e5fff6088ba604a043d7e201534
Michal Wrona <michal.wrona@eclipsegroup.co.uk>
2023-08-16 20:39:36 +0200

    __serialize() example

--- pub/index.php
+++ pub/index.php
@@ -1,9 +1,23 @@
 <?php
-echo 'Hello World!';
+echo 'Magic Methods';
 
 require ('../vendor/autoload.php');
 
-use MichalWrona\PhpStarter\Client;
+use MichalWrona\PhpStarter\Car;
 
-$magic = new Client();
-print $magic->execute();
+//__serialize() example
+
+$car = new Car();
+$car->setBrand('Chevrolet');
+$car->setModel('Tahoe');
+
+print_r('<pre>');
+print_r(serialize($car));
+print_r('</pre>');
+/**
+ * without __serialize() implemented in Car
+ * O:26:"MichalWrona\PhpStarter\Car":2:{s:33:"MichalWrona\PhpStarter\Carbrand";s:9:"Chevrolet";s:33:"MichalWrona\PhpStarter\Carmodel";s:5:"Tahoe";}
+ *
+ * with __serialize() implemented in Car
+ * O:26:"MichalWrona\PhpStarter\Car":3:{s:5:"brand";s:9:"Chevrolet";s:5:"model";s:5:"Tahoe";s:5:"color";s:5:"black";}
+ */
---
+++ src/Car.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichalWrona\PhpStarter;
+
+class Car
+{
+    private string $brand;
+
+    private string $model;
+
+    public function __serialize(): array
+    {
+        return [
+            'brand' => $this->getBrand(),
+            'model' => $this->getModel(),
+            'color' => 'black'
+        ];
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function setBrand(string $brand): void
+    {
+        $this->brand = $brand;
+    }
+
+    public function setModel(string $model): void
+    {
+        $this->model = $model;
+    }
+}
