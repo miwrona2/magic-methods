6126593246349daa2938d7cab5fc3d9671610e7c
Michal Wrona <michal.wrona@eclipsegroup.co.uk>
2023-08-17 16:49:19 +0200

    _call -> company

--- pub/index.php
+++ pub/index.php
@@ -1,11 +1,14 @@
 <?php
-echo 'Magic Methods';
 
 require('../vendor/autoload.php');
 
 use MichalWrona\PhpStarter\Car;
+use MichalWrona\PhpStarter\Company;
+
+print '<h1>Magic Methods in PHP</h1>' . PHP_EOL;
 
 //__serialize() example
+print '<h3>__serialize</h3>' . PHP_EOL;
 
 $car = new Car();
 $car->setBrand('Chevrolet');
@@ -24,15 +27,36 @@ print_r('</pre>');
 
 
 //__call example
+print '<h3>__call</h3>' . PHP_EOL;
 
-$car->setData('Type', 'SUV');
-print_r('<pre>');
-print_r($car->getType());
-print_r('</pre>');
+$company = new Company();
 
-$car->setProductionYear(2011);
-print_r('<pre>');
-print_r($car->getProductionYear());
-print_r('</pre>');
+//1. Getters and setters declared in class Company
+$company->setName('Allegro');
+$company->setIndustry('E-commerce');
+print $company->getName();
+//Allegro
 
-var_dump($car->getData());
+print '</br>';
+
+print $company->getIndustry();
+//E-commerce
+
+print '</br>';
+
+//2. Getter 'getType', 'setType' not declared in class Company
+$company->setData('Type', 'SUV');
+print $company->getType();
+//SUV
+
+print '</br>';
+
+//3. Getter and setter pair 'getProductionYear', 'setProductionYear' not declared in class Company
+$company->setProductionYear(2011);;
+print $company->getProductionYear();
+//2011
+
+print '</br>';
+
+var_dump($company->getData());
+//array(2) { ["Type"]=> string(3) "SUV" ["ProductionYear"]=> int(2011) }
---
+++ src/Company.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichalWrona\PhpStarter;
+
+class Company
+{
+    private string $name;
+
+    private string $industry;
+
+    private array $data;
+
+    public function __call(string $name, array $arguments)
+    {
+        return match (substr($name, 0, 3)) {
+            'set' => $this->data[substr($name, 3)] = $arguments[0] ?: null,
+            'get' => $this->data[substr($name, 3)],
+            default => 'Method' . $name . ' does not exist on class ' . self::class,
+        };
+    }
+    public function setData(string $key, mixed $value): self
+    {
+        $this->data[$key] = $value;
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getIndustry(): string
+    {
+        return $this->industry;
+    }
+
+    public function setIndustry(string $industry): void
+    {
+        $this->industry = $industry;
+    }
+}
